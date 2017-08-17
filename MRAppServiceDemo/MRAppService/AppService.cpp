c7debe26c6584921eda48cd791cf24273b639c26
Dale Stammen <dalestam@microsoft.com>
2017-08-17 10:06:14 -0700

    added Mixed Reality AppService example

---
+++ MRAppServiceDemo/MRAppService/AppService.cpp
@@ -0,0 +1,57 @@
+﻿#include "pch.h"
+#include "AppService.h"
+#include <ppltasks.h>    
+
+using namespace concurrency;
+using namespace MRAppService;
+using namespace Platform;
+using namespace Windows::ApplicationModel::AppService;
+using namespace Windows::ApplicationModel::Background;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+
+AppService::AppService()
+	: m_requestCount(0)
+{
+}
+
+void AppService::Run(IBackgroundTaskInstance^ taskInstance)
+{
+	// Get a deferral so that the service isn't terminated.
+	m_backgroundTaskDeferral = taskInstance->GetDeferral();
+
+	// Associate a cancellation handler with the background task.
+	taskInstance->Canceled += ref new BackgroundTaskCanceledEventHandler(this, &AppService::OnTaskCanceled);
+		
+	// Retrieve the app service connection and set up a listener for incoming app service requests.
+	auto details = (AppServiceTriggerDetails^)taskInstance->TriggerDetails;
+	m_appServiceconnection = details->AppServiceConnection;
+	m_appServiceconnection->RequestReceived += ref new TypedEventHandler<AppServiceConnection^, AppServiceRequestReceivedEventArgs^>(this, &AppService::OnRequestReceived);
+}
+
+void AppService::OnRequestReceived(AppServiceConnection^ sender, AppServiceRequestReceivedEventArgs^ args)
+{
+	// Get a deferral because we use an async API below to respond to the message
+	// and we don't want this call to get cancelled while we are waiting.
+	auto messageDeferral = args->GetDeferral();
+
+	ValueSet^ message = args->Request->Message;
+	ValueSet^ returnData = ref new ValueSet();
+
+	returnData->Insert(L"Result", ++m_requestCount);
+
+	create_task(args->Request->SendResponseAsync(returnData)).then([messageDeferral](AppServiceResponseStatus response)
+	{
+		messageDeferral->Complete();
+	});
+}
+
+void AppService::OnTaskCanceled(IBackgroundTaskInstance^ sender, BackgroundTaskCancellationReason reason)
+{
+	if (m_backgroundTaskDeferral != nullptr)
+	{
+		// Complete the service deferral.
+		m_backgroundTaskDeferral->Complete();
+	}
+}
+
---
+++ MRAppServiceDemo/MRAppService/AppService.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+namespace MRAppService
+{
+	[Windows::Foundation::Metadata::WebHostHidden]
+	public ref class AppService  sealed : public Windows::ApplicationModel::Background::IBackgroundTask
+    {
+    public:
+		AppService();
+
+		virtual void Run(Windows::ApplicationModel::Background::IBackgroundTaskInstance^ taskInstance);
+
+	private:
+		void OnRequestReceived(Windows::ApplicationModel::AppService::AppServiceConnection^ sender, Windows::ApplicationModel::AppService::AppServiceRequestReceivedEventArgs^ args);
+		void OnTaskCanceled(Windows::ApplicationModel::Background::IBackgroundTaskInstance^ sender, Windows::ApplicationModel::Background::BackgroundTaskCancellationReason reason);
+
+		Platform::Agile<Windows::ApplicationModel::Background::BackgroundTaskDeferral> m_backgroundTaskDeferral = nullptr;
+		Windows::ApplicationModel::AppService::AppServiceConnection^ m_appServiceconnection = nullptr;
+		unsigned int m_requestCount;
+    };
+}
